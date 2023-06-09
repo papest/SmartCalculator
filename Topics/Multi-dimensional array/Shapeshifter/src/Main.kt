c4f5d73a8a887f2285eecfbc790173f1470b1668
Epap <peia@inbox.ru>
2023-06-09 16:12:35 +0300

    Multidimensional array

---
+++ Topics/Multi-dimensional array/Shapeshifter/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var inputArray: Array<Array<String>> = arrayOf()
+    val n = readLine()!!.toInt()
+    for (i in 0 until n) {
+        val strings = readLine()!!.split(' ').toTypedArray()
+        inputArray += strings
+    }
+    println(arrayOf(inputArray.last(), inputArray.first()).contentDeepToString())
+}
