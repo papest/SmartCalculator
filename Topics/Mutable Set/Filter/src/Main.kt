314f0d011d7eeac012db1d65257219c484ccec11
Epap <peia@inbox.ru>
2023-06-16 08:59:43 +0300

    Solution with MutableSet

--- Topics/Mutable Set/Filter/src/Main.kt
+++ Topics/Mutable Set/Filter/src/Main.kt
@@ -1,1 +1,7 @@
-fun evenFilter(numbers: Set<Int>): Set<Int> = numbers.filter { it % 2 == 0 }.toSet()
+fun evenFilter(numbers: Set<Int>): Set<Int> {
+    val result = mutableSetOf<Int>()
+    for (i in numbers) {
+        if (i % 2 == 0) result.add(i)
+    }
+    return result
+}
