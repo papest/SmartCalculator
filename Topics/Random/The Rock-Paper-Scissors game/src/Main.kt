c993471fa3e8437b8866854c115b7e67a31c8e9a
Epap <peia@inbox.ru>
2023-06-17 07:50:06 +0300

    Random from array

--- Topics/Random/The Rock-Paper-Scissors game/src/Main.kt
+++ Topics/Random/The Rock-Paper-Scissors game/src/Main.kt
@@ -1,6 +1,2 @@
-import kotlin.random.Random
 @Suppress("All")
-fun makeDecision(): String {
-    val decision = arrayOf("Rock", "Paper", "Scissors")
-    return decision[Random.nextInt(0, 3)]
-}
+fun makeDecision(): String = arrayOf("Rock", "Paper", "Scissors").random()
