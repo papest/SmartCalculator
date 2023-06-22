1ac7d939af1a266a82c9ced1fb534a51b7b56894
Epap <peia@inbox.ru>
2023-06-22 19:00:12 +0300

    Smart Calculator with variables

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -1,21 +1,16 @@
 package calculator
 
+import java.util.regex.Pattern as P
+
 fun main() {
 
-    while (with(readln()) {
+    while (with(readln().replace(" ", "")) {
             when {
-                this == "/exit" -> "Bye!"
                 isEmpty() -> ""
-                this == "/help" -> "The program calculates the sum an sub of numbers"
-                matches("\\s*/.*".toRegex()) -> "Unknown command"
-                !matches("\\s*[-+]?\\s*\\d+\\s*(([+-]+\\s*\\d+\\s*)?)*".toRegex()) -> "Invalid expression"
-                else ->
-                    replace("+", "")
-                        .replace("--", "")
-                        .replace("^\\s+".toRegex(), "")
-                        .replace("-\\s+".toRegex(), "-")
-                        .split("\\s+".toRegex())
-                        .sumOf { it.toInt() }
+                matches("(([a-zA-Z]+\\d+.*)|(\\d+[a-zA-Z]+.*))".toRegex()) -> "Invalid identifier"
+                matches("/.*".toRegex()) -> command(this)
+                matches("[a-zA-Z]+=.*".toRegex()) -> assigment(this)
+                else -> expression(this)
             }
         }.apply {
             if (this != "") println(this)
@@ -26,4 +21,65 @@ fun main() {
          */
 
     }
+}
+
+fun expression(s: String): String {
+    return with(s) {
+        when {
+            matches("(\\d+[a-zA-Z]+.*) | ([a-zA-Z]+\\d+.*)".toRegex()) -> "Invalid identifier"
+            !matches("[-+]?(\\d+|[a-zA-Z]+)+(([+-]+(\\d+|[a-zA-Z]+))?)*".toRegex()) -> "Invalid expression"
+            else -> {
+                var result = s
+                val validName: P = P.compile("[a-zA-z]+")
+                var matcher = validName.matcher(result)
+                while (matcher.find()) {
+                    if (variables.contains(matcher.group(0))) {
+                        result = result.replaceFirst(matcher.group(0), variables[matcher.group(0)].toString())
+                        matcher = validName.matcher(result)
+
+                    } else {
+                        return "Unknown variable"
+
+                    }
+                }
+                result = result.replace("(--)+".toRegex(), "+")
+                    .replace("\\++".toRegex(), "+")
+                    .replace("\\+-|-\\+".toRegex(), "-")
+                    .replace("\\+".toRegex(), " ")
+                    .replace("(?<=\\d)-".toRegex(), " -")
+                    .split(" ")
+                    .sumOf { it.toInt() }.toString()
+                result
+            }
+        }
+
+    }
+}
+
+val variables = mutableMapOf<String, Int>()
+fun assigment(s: String): String {
+    return with(s) {
+        val pos = indexOf('=')
+        val name = substring(0, pos)
+        if (length > pos + 1) {
+            expression(substring(pos + 1))
+                .run {
+
+                    if (matches("-?\\d+".toRegex())) {
+                        variables[name] = this.toInt()
+                        ""
+                    } else {
+                        "Invalid assigment"
+                    }
+                }
+        } else {
+            "Invalid assigment"
+        }
+    }
+}
+
+fun command(s: String): String = when (s) {
+    "/exit" -> "Bye!"
+    "/help" -> "The program calculates the sum an sub of numbers"
+    else -> "Unknown command"
 }
